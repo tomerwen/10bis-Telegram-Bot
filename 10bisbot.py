004fb746874cb730b0ff6735905aa94196ad1fd6
Jaanuk32 <tomerwen@users.noreply.github.com>
2022-10-31 19:24:28 +0200

    added files and .gitignore

---
+++ 10bisbot.py
@@ -0,0 +1,12 @@
+import telegram
+import asyncio
+
+
+async def main():
+    bot = telegram.Bot("5757017167:AAGvkM6Sow6IKdQsf4ZHWvlATGVCwQiEZrE")
+    async with bot:
+        print(await bot.get_me())
+
+
+if __name__ == '__main__':
+    asyncio.run(main())

1bc554d655d44687ae7ba21a6367298f8afa91f9
Jaanuk32 <tomerwen@users.noreply.github.com>
2022-11-01 20:03:01 +0200

    Fixed download images

--- 10bisbot.py
+++ 10bisbot.py
@@ -1,16 +1,38 @@
 import telebot
 import credentials
+from io import BytesIO
+import logging
+import requests
+from PIL import Image
+from os import path
+
+#Variables
+voucher_number = 0
+Path = credentials.Path
+
 
 
 bot = telebot.TeleBot(credentials.bot_token)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-	bot.reply_to(message, "Hello World, use /help to learn more commands")
+    bot.reply_to(message, "Hello World, use /help to learn more commands")
 
 @bot.message_handler(commands=['version'])
 def version(message):
     bot.reply_to(message, "1.0")
+
+
+@bot.message_handler(content_types=['photo'])
+def add(message):
+    fileID = message.photo[-1].file_id
+    file_info = bot.get_file(fileID)
+    file = requests.get(f'https://api.telegram.org/file/bot{credentials.bot_token}/{fileID}'.format(credentials.bot_token, file_info.file_path)) 
+    downloaded_file = bot.download_file(file_info.file_path)
+    with open(path.join(Path , f"{voucher_number}.jpg"), 'wb') as new_file:
+        new_file.write(downloaded_file)
+    bot.reply_to(message, f"Added as {voucher_number}.jpg")
+    
     
 @bot.message_handler(commands=['help'])
 def help(message):

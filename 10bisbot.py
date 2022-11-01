b958992660a972e99c333d62184eb5f8fe438e18
Jaanuk32 <tomerwen@users.noreply.github.com>
2022-11-01 16:58:49 +0200

    fixed token

--- 10bisbot.py
+++ 10bisbot.py
@@ -1,12 +1,19 @@
-import telegram 
-import asyncio
+import logging
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
 
-async def main():
-    bot = telegram.Bot("5757017167:AAGvkM6Sow6IKdQsf4ZHWvlATGVCwQiEZrE")
-    async with bot:
-        print(await bot.get_me())
-
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    application = ApplicationBuilder().token('5757017167:AAGvkM6Sow6IKdQsf4ZHWvlATGVCwQiEZrE').build()
+    
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+    
+    application.run_polling()

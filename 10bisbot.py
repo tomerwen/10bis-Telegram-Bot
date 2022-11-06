1643748b747ae1cbdf61b7d8b2c397f546d51e10
Jaanuk32 <tomerwen@users.noreply.github.com>
2022-11-06 19:23:11 +0200

    Fixed naming for add

--- 10bisbot.py
+++ 10bisbot.py
@@ -1,16 +1,19 @@
+from turtle import down
 import telebot
 import credentials
 from io import BytesIO
-import logging
 import requests
 from PIL import Image
-from os import path
+from os import path, rename
+import base64
+import cv2
+from pyzbar.pyzbar import decode
+from datetime import datetime
 
 #Variables
-voucher_number = 0
+
 Path = credentials.Path
-
-
+voucher_number = 0
 
 bot = telebot.TeleBot(credentials.bot_token)
 
@@ -25,13 +28,17 @@ def version(message):
 
 @bot.message_handler(content_types=['photo'])
 def add(message):
+    now = datetime.now()
+    current_time = now.strftime("%d%b%H-%M-%S")
     fileID = message.photo[-1].file_id
     file_info = bot.get_file(fileID)
     file = requests.get(f'https://api.telegram.org/file/bot{credentials.bot_token}/{fileID}'.format(credentials.bot_token, file_info.file_path)) 
     downloaded_file = bot.download_file(file_info.file_path)
-    with open(path.join(Path , f"{voucher_number}.jpg"), 'wb') as new_file:
-        new_file.write(downloaded_file)
-    bot.reply_to(message, f"Added as {voucher_number}.jpg")
+    with open(path.join(Path , f"{current_time}.jpg"), 'wb') as new_file:
+        new_file.write(downloaded_file) 
+    bot.reply_to(message, f"Added as {current_time}.jpg")
+    
+
     
     
 @bot.message_handler(commands=['help'])
@@ -42,8 +49,7 @@ def help(message):
     /start    ->    Replies with Hello world
     /version  ->    Shows the current released version
     /help     ->    Thats how you got here
-    """
-                 
+    """         
                  )
 
 bot.infinity_polling()
